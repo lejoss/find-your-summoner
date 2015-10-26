38756a91dcb76ffd8b7b71e7b26a98ba352df1f1
lejoss <abaez@sumset.com>
2015-10-26 15:08:54 -0500

    controller new feature: get last 5 matches from summoner Id service added new endpoints for match details and champions

--- app/www/js/controllers/home-search.js
+++ app/www/js/controllers/home-search.js
@@ -5,23 +5,26 @@
         .module('league-finder')
         .controller('HomeSearch', HomeSearch);
 
-    HomeSearch.$inject = ['$scope', '$ionicPopup', 'summonerService', '$q'];
+    HomeSearch.$inject = ['$scope', '$ionicPopup', 'leagueService', '$q'];
 
-    function HomeSearch($scope, $ionicPopup, summonerService, $q) {
+    function HomeSearch($scope, $ionicPopup, leagueService, $q) {
 
-        $scope.summoner           = null;   // summoner details
-        $scope.summonerInput      = null;   // input used to query summoners data
-        $scope.data               = {
+        $scope.vm                    = [];   // view model used to display card data on screen
+
+        $scope.summoner              = null; // summoner details
+        $scope.summonerInput         = null; // input used to query summoners data
+        $scope.data                  = {
             userRegion: null
         };
 
-        // lists
+        // raw lists for transform
+        $scope.matches               = [];   // raw list of summoner's matches when promise resolved
+        $scope.detailMatches         = [];   // raw list of summoner's detail matches when promise resolved
+        $scope.champions             = [];   // raw list of champions involved in last games when promise resolved
+        $scope.stats                 = [];   // raw list of stats from the last matches
 
-        $scope.matches            = [];     // raw list of summoner's matches
-        $scope.champions          = [];     // raw list of champions involved in last games
-        $scope.championPromises   = [];     // array of promises used to return $q.all()
-        $scope.lastMatches        = [];     // array to be displayed on view
-        $scope.regions            = [
+        // hardcoded list
+        $scope.regions               = [
             { name: "Brazil", value: "br" },
             { name: "Europe Nordic & East", value: "eune" },
             { name: "Europe West", value: "euw" },
@@ -36,8 +39,8 @@
         ];  // lol regions
 
         // methods
-        $scope.selectRegion       = selectRegion;
-        $scope.getLastMatches     = getLastMatches;
+        $scope.selectRegion         = selectRegion;
+        $scope.getLastMatches       = getLastMatches;
 
         //////////////////////////////////////////////////////////////////////////////////
 
@@ -50,7 +53,7 @@
                     { text: 'Accept',
                       type: 'button-positive',
                         onTap: function(e) {
-                            _.forEach($scope.regions, function(region) {
+                            _.filter($scope.regions, function(region) {
                                 if (region.value == $scope.data.userRegion) {
                                     $scope.regionName = region.name
                                 }
@@ -70,36 +73,104 @@
         }
 
         function getLastMatches() {
-              return summonerService.getSummoner($scope.data.userRegion, $scope.summonerInput)
 
-                  // promise result of fetching summoner
-                .then(function(result) {
-                      $scope.summoner = result.data;
-                      return summonerService.getLast5Games($scope.data.userRegion, _.pluck($scope.summoner, 'id'));
-                })
-                  // promise result of fetching user's last 5 games
-                .then(function(result) {
-                      $scope.matches = result.data.matches;
+          return leagueService.getSummoner($scope.data.userRegion, $scope.summonerInput)
+              // promise result of fetching summoner
+            .then(function(result) {
+                  $scope.summoner = result.data;
+                  $scope.summonerId = _.pluck($scope.summoner, 'id');
 
-                      _.forEach($scope.matches, function(match) {
-                          $scope.championPromises.push(summonerService.getChampionById($scope.data.userRegion, match.champion));
-                      });
-                      return $q.all($scope.championPromises);
-                })
-                  // promise result of fetching champions in matches
-                .then(function(result) {
-                      $scope.champions = result;
-                      var championMatch;
+                  return leagueService.getLast5Games($scope.data.userRegion, $scope.summonerId);
+            })
+              // promise result of fetching summoner's last matches
+            .then(function(result) {
+                  $scope.matches = result.data.matches;
+                  var matchPromises = [];
 
-                      _.forEach($scope.champions, function(champion) {
-                          championMatch = _.find($scope.matches, { champion : champion.data.id});
-                          if (championMatch) {
-                              championMatch.championName = champion.data.name;
-                              $scope.lastMatches.push(_.pick(championMatch, ['championName', 'lane', 'queue', 'timestamp']));
+                  $scope.matches =
+                      _.map($scope.matches, function (match) {
+                          matchPromises.push(leagueService.getMatchDetails($scope.data.userRegion, match.matchId));
+                          return {
+                              matchId: match.matchId,
+                              champion: match.champion,
+                              lane: match.lane,
+                              queue: match.queue,
+                              timestamp: match.timestamp
                           }
                       });
-                      $scope.summonerInput = null;
-                })
+
+                  return $q.all(matchPromises);
+            })
+              // promise result of fetching match details
+            .then(function(result) {
+                  $scope.detailMatches = result;
+                  var stats = [];
+                  var championPromises = [];
+                  var currentParticipantId;
+
+                  _.map($scope.detailMatches, function (match) {
+
+                      // summoner current Id in match
+                      currentParticipantId =
+                          _.pluck(_.filter(match.data.participantIdentities, function (participant) {
+                              return participant.player.summonerId == $scope.summonerId;
+                      }), 'participantId').join();
+
+                      // get stats for current participant in match
+                      _.filter(match.data.participants, function (participant) {
+                          if(participant.participantId == currentParticipantId) {
+                              stats.push({
+                                  matchId: match.data.matchId,
+                                  winner: participant.stats.winner,
+                                  kills: participant.stats.kills,
+                                  deaths: participant.stats.deaths,
+                                  assists: participant.stats.assists,
+                                  minionsKilled: participant.stats.minionsKilled,
+                                  champLevel: participant.stats.champLevel
+                              });
+                          }
+                      });
+                  });
+                  $scope.stats = stats;
+                  stats = null;
+
+                  // from summoner match get champion played
+                  _.forEach($scope.matches, function(match) {
+                      championPromises.push(leagueService.getChampionById($scope.data.userRegion, match.champion));
+                  });
+
+                  return $q.all(championPromises);
+              })
+              // promise result of fetching used champions in last matches
+            .then(function(result) {
+                  $scope.champions = result;
+
+                  // last champions played
+                  $scope.champions =
+                      _.map($scope.champions, function(champ) {
+                          return {
+                              champId: champ.data.id,
+                              champName: champ.data.name,
+                              champTitle: champ.data.title
+                          }
+                      });
+
+                  var matchWithStats =
+                      _.map($scope.matches, function(match) {
+                          return _.extend(match, _.omit(_.findWhere($scope.stats, { matchId: match.matchId}), 'matchId'));
+                      });
+
+                  // vm = view model
+                  $scope.vm =
+                      _.map(matchWithStats, function(match) {
+                          return _.extend(match, _.omit(_.findWhere($scope.champions, { champId: match.champion }), 'champId'));
+                      });
+
+                  console.log($scope.vm);
+                  matchWithStats = null;
+                  $scope.summonerInput = null;
+            })
+            .catch();
         }
     }
 })();
--- app/www/js/services/leagueService.js
+++ app/www/js/services/leagueService.js
@@ -6,26 +6,30 @@
 
     angular
         .module('league-finder')
-        .service('summonerService', summonerService);
+        .service('leagueService', leagueService);
 
-    summonerService.$inject = ['$http', 'API_KEY'];
+    leagueService.$inject = ['$http', 'API_KEY'];
 
-    function summonerService($http, API_KEY) {
+    function leagueService($http, API_KEY) {
 
         var service = {
             getSummoner: getSummoner,
             getLast5Games: getLast5Games,
-            getChampionById: getChampionById
+            getChampionById: getChampionById,
+            getMatchDetails: getMatchDetails
         }
 
         function getLast5Games(region, summonerId) {
-            return $http.get('https://lan.api.pvp.net/api/lol/'+ region + '/v2.2/matchlist/by-summoner/'+ summonerId + '?beginIndex=0&endIndex=5&api_key=' + API_KEY);
+            return $http.get('https://lan.api.pvp.net/api/lol/'+ region + '/v2.2/matchlist/by-summoner/'+ summonerId + '?rankedQueues=RANKED_SOLO_5x5,RANKED_TEAM_3x3,RANKED_TEAM_5x5&beginIndex=0&endIndex=5&api_key=' + API_KEY);
         }
         function getSummoner(region, summonerName) {
             return $http.get('https://lan.api.pvp.net/api/lol/' + region + '/v1.4/summoner/by-name/' + summonerName + '?api_key=' + API_KEY);
         }
         function getChampionById(region, championId) {
             return $http.get('https://global.api.pvp.net/api/lol/static-data/'+ region +'/v1.2/champion/'+ championId +'?api_key=' +API_KEY);
+        }
+        function getMatchDetails(region, matchId) {
+            return $http.get('https://lan.api.pvp.net/api/lol/' + region + '/v2.2/match/' + matchId+ '?api_key=' + API_KEY);
         }
 
         return service;
